452bb7c2f5dd1797625e1598327f4f8e42b31702
Wiseman3AM <amukelani210@gmail.com>
2024-07-07 22:53:22 +0200

    draft commit

---
+++ alpine.js
@@ -0,0 +1,7 @@
+const successMessage = document.getElementById("successMessage");
+
+if (successMessage) {
+  setTimeout(() => {
+    successMessage.style.display = "none";
+  }, 5000);
+}
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-/*incrementButton0.addEventListener('click', function(){
-    smallCounter++;
-    document.getElementById('smallInput').value = smallCounter;
-});*/
-
 /* Define Variables
 --------------------------------------------------------------------------------------------------------------------*/
-
 let smallCounter = 0;
 let meduimCounter = 0;
 let largeCounter = 0;
@@ -20,8 +14,26 @@ const decrementButton1 = document.querySelector('.decrement1');
 const incrementButton2 =  document.querySelector('.increment2');
 const decrementButton2 = document.querySelector('.decrement2');
 
-const revealButton = document.getElementById('revealCheckout')
-const hiddenDiv = document.getElementById('hiddenCheckOut')
+const buyButton0 = document.querySelector('.buyButton0')
+const buyButton1 = document.querySelector('.buyButton1')
+const buyButton2 = document.querySelector('.buyButton2')
+
+const revealButton = document.getElementById('revealCheckout');
+const hiddenDiv = document.getElementById('hiddenCheckOut');
+const checkoutDiv = document.querySelector('.checkOut');
+
+const h3Elements0 = document.getElementById('greenGO');
+const h3Elements1 = document.getElementById('redStop');
+const h3Elements2 = document.getElementById('orangeWow');
+
+const inputPayment = document.querySelector(".payAmount");
+const enteredAmount = document.querySelector(".payRecord");
+const payButton = document.querySelector(".payButton");
+const buyButton = document.querySelector(".buyButton")
+
+const totalValue = document.getElementById('totalValue');
+
+
 
 /* Increment Buttons 
 --------------------------------------------------------------------------------------------------------------------*/
@@ -29,21 +41,24 @@ incrementButton0.addEventListener('click', function(){
     totalCost+= 31.99;
     smallCounter++;
     document.getElementById('smallInput').value = smallCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
 });
 
 incrementButton1.addEventListener('click', function(){
     meduimCounter++;
     totalCost+= 58.99;
     document.getElementById('mediumInput').value = meduimCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
 });
 
 incrementButton2.addEventListener('click', function(){
     largeCounter++;
     totalCost += 87.99;
     document.getElementById('largeInput').value = largeCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
 });
 
 /* Decrement Buttons 
@@ -56,7 +71,9 @@ decrementButton0.addEventListener('click', function(){
         totalCost = 0; // Ensure totalCost does not go below zero
     } };
     document.getElementById('smallInput').value = smallCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
+    showCheckoutButton();
 });
 
 decrementButton1.addEventListener('click', function(){
@@ -67,7 +84,8 @@ decrementButton1.addEventListener('click', function(){
         totalCost = 0; // Ensure totalCost does not go below zero
     }};
     document.getElementById('mediumInput').value = meduimCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
 });
 
 decrementButton2.addEventListener('click', function(){
@@ -78,7 +96,8 @@ decrementButton2.addEventListener('click', function(){
         totalCost = 0; // Ensure totalCost does not go below zero
     }};
     document.getElementById('largeInput').value = largeCounter;
-    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2)
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2)
 });
 
 
@@ -91,3 +110,72 @@ revealButton.addEventListener('click', function(){
 
 /* Payment INPUT 
 --------------------------------------------------------------------------------------------------------------------*/
+
+payButton.addEventListener('click', function(){
+    enteredAmount.innerText = "R " + inputPayment.value;
+    if (enteredAmount.innerText === document.getElementById('totalInput').value){
+        h3Elements0.style.display = 'block';
+        h3Elements1.style.display = 'none';
+        h3Elements2.style.display = 'none';
+    } else if (enteredAmount.innerText < document.getElementById('totalInput').value){
+        h3Elements0.style.display = 'none';
+        h3Elements1.style.display = 'block';
+        h3Elements2.style.display = 'none';
+    } else if (enteredAmount.innerText > document.getElementById('totalInput').value){
+        h3Elements0.style.display = 'none';
+        h3Elements1.style.display = 'none';
+        h3Elements2.style.display = 'block';
+    } 
+    }); 
+
+    buyButton0.addEventListener('click', function(){
+          
+          totalCost+= 31.99;
+          smallCounter++;
+          document.getElementById('smallInput').value = smallCounter;
+          document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+          document.getElementById('totalValue').value = "R " + totalCost.toFixed(2);
+      }
+    );
+
+
+      buyButton1.addEventListener('click', function(){
+        meduimCounter++;
+        totalCost+= 58.99;
+        document.getElementById('mediumInput').value = meduimCounter;
+        document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+        document.getElementById('totalValue').value = "R " + totalCost.toFixed(2);
+      });
+
+      buyButton2.addEventListener('click', function(){
+        largeCounter++;
+    totalCost += 87.99;
+    document.getElementById('largeInput').value = largeCounter;
+    document.getElementById('totalInput').value = "R " + totalCost.toFixed(2);
+    document.getElementById('totalValue').value = "R " + totalCost.toFixed(2);
+
+        });
+
+        
+      
+       
+          
+          function displayCheckOut() {
+            const checkoutDiv = document.getElementById('checkIT');
+          
+            if (totalValue.value === "") {
+              checkoutDiv.style.display = 'none';
+            } else {
+              checkoutDiv.style.display = 'block';
+            }
+          };
+
+ totalValue.addEventListener('change', function() {
+            displayCheckOut();
+          });
+
+
+
+/*
+
+          
